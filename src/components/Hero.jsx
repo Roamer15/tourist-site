ee16f7e4e21bbcb9a9fef77cff2fbcd851537ab7
Roamer15 <ianseeksbeta@gmail.com>
2025-02-07 16:56:34 +0100

    Creation Navbar component and start of Hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,10 @@
+export default function Hero() {
+    return (
+        <>
+          <div className="heading">
+            <h1>Experience the beauty of Rwanda with us</h1>
+            <p>Discover the Land of a Thousand Hills</p>
+          </div>
+        </>
+    )
+} 
