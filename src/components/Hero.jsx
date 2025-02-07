8a96252035c5e662485e99a92931c6e235e64953
Roamer15 <ianseeksbeta@gmail.com>
2025-02-07 17:25:57 +0100

    Hero component 60% complete

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,17 @@
 export default function Hero() {
-    return (
-        <>
-          <div className="heading">
-            <h1>Experience the beauty of Rwanda with us</h1>
-            <p>Discover the Land of a Thousand Hills</p>
-          </div>
-        </>
-    )
-} 
+  return (
+    <>
+      <div className="hero">
+        <div className="heading">
+          <h1><b>Experience the beauty of Rwanda with us</b></h1>
+          <p>Discover the Land of a Thousand Hills</p>
+          <div className="navig-icons">
+          <span>&lt;</span>
+          <span>&gt;</span>
+        </div>
+        </div>
+        
+      </div>
+    </>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import HeadContact from "../components/HeadContact"
 import Navbar from "../components/Navbar"
+import Hero from '../components/Hero'
 
 export default function Home() {
     return (
         <>
            <HeadContact />
            <Navbar />
+           <Hero />
         </>
         
     )
