7555a90deae9487ddd2b15ce17fdb5542b61d54e
Roamer15 <ianseeksbeta@gmail.com>
2025-02-28 13:13:57 +0100

    C

--- src/components/GalleryImages.jsx
+++ src/components/GalleryImages.jsx
@@ -61,31 +61,31 @@ export default function GalleryImages() {
         <img src="https://s3-alpha-sig.figma.com/img/24ba/eb3d/49c149077e65e33a96fb00f428230432?Expires=1740355200&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=M4LPcBkUgay9d2c~K~qfDlQG-4JeZzcygIGT-xZPt8Q0fvFWQyvvZHRzNwzoCNIQ8I1iS7bTre0B7lG5GSeVlxEHQlyICb01mbtO09HyWhE0lNo866VoWxWSm5Zt6pidrM93cl4nh89fzzGfpWJ3Esn-98dr91uWvjrLzPJsaoV2-tMXnUaFj0YuD828mmqix2mtL1aKFe4lNsHYUaFOuG2FLauMwOvPIRair4r3W7DqVDGkvXS6qibPEyBmXhUk1SrsFaGo2AsNBOOjy27g8v7bQ20dU6pUGdHb~gBb1oClnFGEzZ~dlta6M9yjm67FbqAVCkUh5DStpvblcwIctQ__" alt="" />
       </div>
       <div className='gallery-item'>
-        <img src="https://s3-alpha-sig.figma.com/img/5e52/ced2/071eff545d1b16dd4bc36fd771859d9d?Expires=1740355200&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=Pa5vxbc4kMbof~rHsfJKGFrrmH~vS0ppcGdJLpPT51e9K3SmDIgY2RfsNy5wlV7FzDfGLzpCD2r8sh9Ws3zziDBEXHV8zD9UE7NtP84nkKug-uQOAIujhmrJF9jyGXGRDUq58hliaNenspXZE9ktRgIzdjSgV9vACWKf33qEX8SCMAFGswEIQidNa6ZUb8qQi6HoPstp4s0pJSigOmtTc6~9~~FXIOj5lGvU9hCysSrlZc94xWl9qMTrpJQRNX8zgQEmpEosqW2Syb3rfiKyoXyfC-bZ5sSuUJn-fv6FCI834YYuSI7RqspfWvgn7p8UQajA~SWnOHGSs5LcSvt6bw__" alt="" />
+        <img src="https://i.pinimg.com/236x/bb/fd/d9/bbfdd92a2774fd149b5f91cc9ba9e684.jpg" alt="" />
       </div>
       <div className='gallery-item'>
-        <img src="https://s3-alpha-sig.figma.com/img/c2c3/8cac/5270c8133fe9fbba80ef684203daf896?Expires=1740355200&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=s7kM7GgxbYewNAVRCIDunlXCmiQY97puDbl6cronfVGjbdWUFjhuY~vrRUQvAL7aQ0tvEHzbYIG0jPeLZZRvCdNoN3WHKZ18HJbTFswea3bTlKUttJxJ~YDUWBrCt17dct~ybVChi2FpxcsNX5p~elOrOhROVcrcJmg7P2ZsaPKXVJzdHI8I~qWLoobcbStVMlBMSRei7BeZ1hfU1xWNEhZ5jHGvRvgbCAxP7Bxjc7ojSIi9nKNVzzhB5jYJJ89LAyotxEwxZLOwKG3tHQD5b9QXzmEgDhV7EF4ntmb04qOPnSa3mV2jTAZik19OhsHOTMlVOCEf-r3EGCTGaMHUKQ__" alt="" />
+        <img src="https://i.pinimg.com/236x/69/e1/dc/69e1dc98e52bad3de30f52a5ae89e5ba.jpg" alt="" />
       </div>
       <div className='gallery-item'>
-        <img src="https://s3-alpha-sig.figma.com/img/fc87/170d/90c406720a69ef576defbab016788d2f?Expires=1740355200&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=i6pyWqHlsu-iocA7JpowAi~PYPwvKroFUpyOMIEOyHGpQeNtAISZQh6tre43XKQOW0SP3lVLuUCjm~19ccXDXi9h0TtyvAdwxT-llO35isj0Kk0BejzgQ6tMnqki33pHNiey8EZChqr4Aq~sBSVj-FwRhnxrJ9OxGL~VvWcZLrMum0jN15cHgeBv0nPL49G2Emb5SCaNqZ6EuDSqAnMVPGbRetwOTz4wg4dnmIwyeDCyU0ozf3~4Rvn7zSpeZhtQ2oF6iopSBn~noZROtJ5-0-TWnl6eErZE3ZBK7eGAEo8zizsYMbjc9vYLtXvBWmD7Zx~dvQBBEz6VITS0bmSBPQ__" alt="" />
+        <img src="https://i.pinimg.com/236x/6f/17/93/6f17934ec3d9dda5d43b3f714f90aa3a.jpg" alt="" />
       </div>
       <div className='gallery-item'>
-        <img src="https://s3-alpha-sig.figma.com/img/251d/2fdb/537abdd9e098d432d20e7e633d4c2b73?Expires=1740355200&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=KINJFaw~i1vLXb6nkZciPprKzmAisj6jLoQ6GXC~CQp-QPsoaSYMijn4rBWaax~VqQjUlIOg3MKIN9Pcdv6P0zWzam0qq983tN5h9h3n-tguGypkCSLhWw96Ggr48Amn7jbpZTHORbA7budcvIN~rudO3977I0I2owha99mWhI5AWWOEnXqKsbBhGZ-a94Hz0co5UxtwNB~tnaOLcT06nhOuf8ei-dhD5tqHRmYrex9zLGG1snLST~Gv-09xwvWGsCKGmxaframojMGUJ9~4CBPyvuuAPCeJdRRiIQHtBwxY8k1LFVkY48wdO6EbYG5b2EzE061yUgGrTGVFRFerKQ__" alt="" />
+        <img src="https://i.pinimg.com/236x/f9/da/90/f9da9063465e2fe1e96fe8cd311fddc1.jpg" alt="" />
       </div>
       <div className='gallery-item'>
-        <img src="https://s3-alpha-sig.figma.com/img/7f97/05cf/efd22d0a9bd512eb96d8b30fd398822a?Expires=1740355200&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=duZTFaGarjRrvkMjlW0NKHiBcw2LhUo9bpm-KD3wPDgAnfNvuhs89TiU-KflXdjK0M9dQksT6zuC6PKphRVajFer86pF0EsHVQfxsqqJxI0AKKurTjdfVadDEVy-CbsFhvq0xqZ5n1mhueePaqg6bjyhRffp~VBIhXBxdKAjj7fpICUViNKah8Y-9ovS5nKPvecd-DJTJJAN-PllbID~jTF6c59v6~6xHT7qLI0RsNb9So1atLzSsVIryw9Fi9wmtWz1p82rf4cB~7HbnLdosN1LaWKt~BSA0zPDF1y3TYKWjer84d1K6KHvg3TvbJTh0vqb3I8romfSzV72Bc9zsA__" alt="" />
+        <img src="https://i.pinimg.com/736x/d8/38/ea/d838eae08f05f780193e1e60ebd7a83e.jpg" alt="" />
       </div>
       <div className='gallery-item' style={{height: '250px'}}>
         <img alt="" src="https://lp-cms-production.imgix.net/2023-03/GettyImages-1139556558.jpg" />
       </div>
       <div className='gallery-item'>
-        <img src="https://s3-alpha-sig.figma.com/img/efdb/cec2/a35932c0e5b21dfbfb30629b08803e12?Expires=1740355200&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=juyKdNpTPmn1BKKXXVl1Jw1tvTSIvd4EI-89jV2Usxx5gJvKYS~NCKI60tDiDOL0qRvlEKpGFFW~aitACy9npON81bWLpYI0OUgX8B27xRdEphLiu02rAcMWwHdVrPFffzyGJB2rFv--cEMUrJBFE-36zL3Ghh~yO1uWysTmA87Y4wDzOw9sxZIHAoMsQ9gdt5QfSgBAcsKIGCT82deX~kIY8u8C86GrEDofPwsANPBSU8hz4B-ZdixQRQoAQJwR4E2hrs1IYK~cje~xalc84mDqGv3V5orRcp39imuqCpqjPfAZuP3DWx29fk1zn4~c9RcRzszMK7dmaVCtDczsBg__" alt="" />
+        <img src="https://i.pinimg.com/236x/b0/6d/4a/b06d4aea50c9d6d1fdf8a3b186f963c2.jpg" alt="" />
       </div>
       <div className='gallery-item' style={{height:"500px"}}>
         <img src="https://www.google.com/imgres?q=pics%20of%20rwanda&imgurl=https%3A%2F%2Fmedia.istockphoto.com%2Fid%2F1254053927%2Fphoto%2Flandscape-of-the-virunga-mountains-in-rwanda.jpg%3Fs%3D612x612%26w%3D0%26k%3D20%26c%3DYEFagi2Y1ulPHJckKBeEBdOyMrDW3eLwDHjsCZSGgGo%3D&imgrefurl=https%3A%2F%2Fwww.istockphoto.com%2Fphotos%2Frwanda-landscape&docid=Rc3LgjqFJMWRDM&tbnid=7YGgvPQzDZK1nM&vet=12ahUKEwiIluXO4OGLAxUTRPEDHb_LE_YQM3oECGcQAA..i&w=612&h=408&hcb=2&ved=2ahUKEwiIluXO4OGLAxUTRPEDHb_LE_YQM3oECGcQAA" alt="" />
       </div>
       <div className='gallery-item' style={{height:"500px"}} >
-        <img src="https://www.google.com/imgres?q=pics%20of%20rwanda&imgurl=https%3A%2F%2Fcdn.britannica.com%2F93%2F7193-050-513C8A92%2FFamily-settlements-hillsides-Rwanda.jpg&imgrefurl=https%3A%2F%2Fwww.britannica.com%2Fplace%2FRwanda&docid=QhYPnLsK0Fwn3M&tbnid=NJT11QmKdNG9GM&vet=12ahUKEwiIluXO4OGLAxUTRPEDHb_LE_YQM3oECHcQAA..i&w=1600&h=1074&hcb=2&ved=2ahUKEwiIluXO4OGLAxUTRPEDHb_LE_YQM3oECHcQAA" alt="" />
+        <img src="https://i.pinimg.com/474x/a5/1e/73/a51e738739d5470cd2ca16e147ed5b43.jpg" alt="" />
       </div>
     </div>
   );
